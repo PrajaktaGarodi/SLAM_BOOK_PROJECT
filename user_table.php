a745dff0ffcc050464191f2db7d646d7e9c79cbc
PrajaktaGarodi <prajaktagarodi@gmail.com>
2024-11-06 19:30:33 +0530

    create user table for admin

--- user_table.php
+++ user_table.php
@@ -32,29 +32,14 @@
 
 <body>
     <div class="container-scroller">
-        <div class="row p-0 m-0 proBanner" id="proBanner">
-            <div class="col-md-12 p-0 m-0">
-                <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
-                    <div class="ps-lg-3">
-                        <div class="d-flex align-items-center justify-content-between">
-                            <p class="mb-0 font-weight-medium me-3 buy-now-text">Free 24/7 customer support, updates,
-                                and more with this template!</p>
-                            <a href="https://www.bootstrapdash.com/product/purple-bootstrap-admin-template/"
-                                target="_blank" class="btn me-2 buy-now-btn border-0">Buy Now</a>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between">
-                        <a href="https://www.bootstrapdash.com/product/purple-bootstrap-admin-template/"><i
-                                class="mdi mdi-home me-3 text-white"></i></a>
-                        <button id="bannerClose" class="btn border-0 p-0">
-                            <i class="mdi mdi-close text-white mr-0"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
         <?php
         include('Common/side_bar.php');
+        if ($result['role_id'] == 1) 
+        {
+            $sql = "SELECT * FROM users where role_id = 2;" ;
+            $query = mysqli_query($conn, $sql);
+                    
         ?>
 
         <div class="main-panel">
@@ -63,125 +48,44 @@
                     <div class="col-lg-12 grid-margin stretch-card">
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="card-title">Striped Table</h4>
-                                <p class="card-description"> Add class <code>.table-striped</code>
+                                <h4 class="card-title">Users In slambook</h4>
+                                <p class="card-description"> 
                                 </p>
                                 <table class="table table-striped">
                                     <thead>
                                         <tr>
-                                            <th> User </th>
-                                            <th> First name </th>
-                                            <th> Progress </th>
-                                            <th> Amount </th>
-                                            <th> Deadline </th>
+                                            <th> Profile </th>
+                                            <th> username </th>
+                                            <th> Address </th>
+                                            <th> Phone No </th>
+                                            <th> Status</th>
+                                            <th> Action </th>
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        <?php while($row = mysqli_fetch_assoc($query)) {?>
+
+
                                         <tr>
                                             <td class="py-1">
-                                                <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
+                                                <img src="assets/images/<?php echo $row['profile'] ?>" alt="image" />
                                             </td>
-                                            <td> Herman Beck </td>
-                                            <td>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-success" role="progressbar"
-                                                        style="width: 25%" aria-valuenow="25" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </td>
-                                            <td> $ 77.99 </td>
-                                            <td> May 15, 2015 </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="py-1">
-                                                <img src="../../assets/images/faces-clipart/pic-2.png" alt="image" />
-                                            </td>
-                                            <td> Messsy Adam </td>
-                                            <td>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-danger" role="progressbar"
-                                                        style="width: 75%" aria-valuenow="75" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </td>
-                                            <td> $245.30 </td>
-                                            <td> July 1, 2015 </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="py-1">
-                                                <img src="../../assets/images/faces-clipart/pic-3.png" alt="image" />
-                                            </td>
-                                            <td> John Richards </td>
-                                            <td>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-warning" role="progressbar"
-                                                        style="width: 90%" aria-valuenow="90" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </td>
-                                            <td> $138.00 </td>
-                                            <td> Apr 12, 2015 </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="py-1">
-                                                <img src="../../assets/images/faces-clipart/pic-4.png" alt="image" />
-                                            </td>
-                                            <td> Peter Meggik </td>
-                                            <td>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-primary" role="progressbar"
-                                                        style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </td>
-                                            <td> $ 77.99 </td>
-                                            <td> May 15, 2015 </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="py-1">
-                                                <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
-                                            </td>
-                                            <td> Edward </td>
-                                            <td>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-danger" role="progressbar"
-                                                        style="width: 35%" aria-valuenow="35" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </td>
-                                            <td> $ 160.25 </td>
-                                            <td> May 03, 2015 </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="py-1">
-                                                <img src="../../assets/images/faces-clipart/pic-2.png" alt="image" />
-                                            </td>
-                                            <td> John Doe </td>
-                                            <td>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-info" role="progressbar"
-                                                        style="width: 65%" aria-valuenow="65" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </td>
-                                            <td> $ 123.21 </td>
-                                            <td> April 05, 2015 </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="py-1">
-                                                <img src="../../assets/images/faces-clipart/pic-3.png" alt="image" />
-                                            </td>
-                                            <td> Henry Tom </td>
-                                            <td>
-                                                <div class="progress">
-                                                    <div class="progress-bar bg-warning" role="progressbar"
-                                                        style="width: 20%" aria-valuenow="20" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </td>
-                                            <td> $ 150.00 </td>
-                                            <td> June 16, 2015 </td>
-                                        </tr>
+                                            <td> <?php echo $row['username'] ?> </td>
+                                            <td> <?php echo $row['address'] ?> </td>
+                                            <td> <?php echo $row['contact'] ?> </td>
+                                            <td> <?php echo $row['status'] ?> </td>
+                                            <td>  <a href="user_view.php?user_id=<?php echo $row['user_id']?>"><i class="fa-solid fa-eye"></i></a>
+                                            <a href=""><i class="fa-solid fa-user-pen"></i></a>
+                                            <a href=""><i class="fa-solid fa-trash"> </i></a> 
+                                   </td>
+                                            
+
+                                               
+                                        <?php 
+                                    
+                                    
+                                    }?>
+                                    
                                     </tbody>
                                 </table>
                             </div>
@@ -190,4 +94,7 @@
                 </div>
             </div>
         </div>
+        <?php
+        }
+        ?>
     </div>
---
+++ user_view.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Slam book user view</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="assets/dashboard_vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="assets/dashboard_vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/dashboard_vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="assets/dashboard_vendors/font-awesome/css/font-awesome.min.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="assets/dashboard_vendors/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="assets/dashboard_vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="assets/css/dashboard_css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="assets/dashboard_images/favicon.png" />
+    <style>
+        .error {
+            color: red;
+            font-size: 15px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-scroller">
+
+        <?php
+        include('Common/side_bar.php');
+        if ($result['role_id'] == 1) {
+            $user_id = $_GET['user_id'];
+
+            if (isset($user_id)) {
+                $query = "SELECT * FROM users WHERE user_id = $user_id";
+                $result = mysqli_query($conn, $query);
+                $row = mysqli_fetch_assoc($result);
+
+
+
+            }
+
+            ?>
+
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <div class="container">
+                        <h1>User Details</h1>
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <div class="card-title text-center fw-">
+                                        Profile
+                                    </div>
+                                </div>
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                <img src="assets/images/<?php echo $row['profile'] ?>" class="img-fluid rounded-circle" alt="image" />
+                                <h2 class="text-center mt-2" ><?php echo $row['username'] ?></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
