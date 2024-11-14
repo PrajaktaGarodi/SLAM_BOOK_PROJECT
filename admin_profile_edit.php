79a775d542f9f3b6d543366df4f694915b301f0e
PrajaktaGarodi <prajaktagarodi@gmail.com>
2024-11-14 18:51:10 +0530

    admin profile edit form created

--- admin_profile_edit.php
+++ admin_profile_edit.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Slam book Admin Profile</title>
+    <title>Slam book User Profile Edit</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="assets/dashboard_vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="assets/dashboard_vendors/ti-icons/css/themify-icons.css">
@@ -37,14 +37,29 @@
         include('Common/side_bar.php');
         if ($result['role_id'] == 1) {
 
-                $user_id= $result['user_id'];
+                $user_id = $result['user_id'];
                 $query = "SELECT * FROM users WHERE user_id = $user_id";
                 $result = mysqli_query($conn, $query);
                 $row = mysqli_fetch_assoc($result);
 
 
+                if(isset($_POST['submit']))
+                {
+                    $name = $_POST['name'];
+                    $email = $_POST['email'];
+                    $phone = $_POST['contact'];
+                    $address = $_POST['address'];
+                    $gender = $_POST['gender'];
 
-            
+                    $sql = "UPDATE users SET username = '$name', email = '$email', contact ='$phone',address = '$address', gender = '$gender' WHERE user_id = $user_id;";
+
+                    if (mysqli_query($conn, $sql)) {
+                        echo "<script>alert('Profile updated successfully');</script>";
+                        echo "<script>window.location.href = 'admin_profile.php?id=$user_id'</script>";
+                    } else {
+                        echo "Error updating record: ". mysqli_error($conn);
+                    }
+                }
 
             ?>
 
@@ -53,12 +68,13 @@
                     <div class="container">
                         <h1>Admin Profile</h1>
                         <div class="col-md-12 mt-4">
+
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="card">
                                         <div class="card-header">
                                             <div class="card-title text-center fw-">
-                                                Profile
+                                                 Admin Profile Edit
                                             </div>
                                         </div>
                                         <div class="card-body d-flex flex-column justify-content-center">
@@ -73,82 +89,51 @@
                                     <div class="card">
                                         <div class="card-header">
                                             <div class="card-title text-center">
-                                            Admin Details
+                                                Admin Details
                                             </div>
                                         </div>
                                         <div class="card-body">
+                                            <form method="post">
                                             <div class="row">
                                                 <div class="col-md-6">
+                                                        <div class=""></div>
                                                         <h6 class="text-secondary fw-bold">Name :</h6>
-                                                        <p><?php echo $row['username']; ?></p>
+                                                        <input type="text" class="form-control" name="name" id="name" value="<?php echo $row['username'];?>">
                                                 </div>
                                                 
                                                 <div class="col-md-6">
-                                                    
                                                         <h6 class="text-secondary fw-bold">Email :</h6>
-                                                        <p><?php echo $row['email'];?></p>
-                                                
+                                                        <input type="email" class="form-control" name="email" id="email" value="<?php echo $row['email'];?>">
                                                 </div>
 
                                                 <div class="col-md-6">
                                                     
                                                         <h6 class="text-secondary fw-bold">Contact:</h6>
-                                                        <p><?php echo $row['contact'];?></p>
+                                                        <input type="number" class="form-control" name="contact" id="contact" value="<?php echo $row['contact'];?>">
                                                 
                                                 </div>
 
                                                 <div class="col-md-6">
                                                     
-                                                        <h6 class="text-secondary fw-bold">Adderss:</h6>
-                                                        <p><?php echo $row['address'];?></p>
+                                                        <h6 class="text-secondary fw-bold">Address:</h6>
+                                                        <input type="text" class="form-control" name="address" id="address" value="<?php echo $row['address'];?>">
                                                 
                                                 </div>
 
                                                 <div class="col-md-6">
                                                     
                                                         <h6 class="text-secondary fw-bold">Gender:</h6>
-                                                        <p><?php echo $row['gender'];?></p>
+                                                        <input type="text" class="form-control" name="gender" id="gender" value="<?php echo $row['gender'];?>">
                                                 
                                                 </div>
 
-                                                <div class="col-md-6">
-                                                    
-                                                        <h6 class="text-secondary fw-bold">Account Created on:</h6>
-                                                        <p><?php echo $row['created_at'];?></p>
-                                                
-                                                </div>
+                                                <div class="w-100"></div>
 
-                                                <div class="col-md-6">
-                                                    
-                                                        <h6 class="text-secondary fw-bold">Account Update On:</h6>
-                                                        <p><?php echo $row['updated_at'];?></p>
-                                                
-                                                </div>
-                                                
-                                                <div class="col-md-6">
-                                                    
-                                                        <h6 class="text-secondary fw-bold">Status :</h6>
-                                                        
-                                                            <?php 
-                                                                if ($row['status'] =='active') 
-                                                                {
-                                                                    echo '<p class="text-success"> Active </p>';
-                                                                } 
-                                                                else 
-                                                                {
-                                                                    echo '<p class="text-danger"> Inactive </p>';
-                                                                }
-
-                                                        
-                                                            ?>
-                                                        
-                                                
-                                                </div>
-                                             <div class="col-ms-4">
-                                                <a href="admin_profile_edit.php" class="btn btn-primary "> Edit Admin</a>
-                                            </div>
-
-                                            </div>
+                                                    <div class="col-md-5 mt-4">
+                                                   <button type="submit" class="btn btn-primary" name="submit">Make Changes</button>
+                                                   </div>
+                                                 </div>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
@@ -157,7 +142,7 @@
                     </div>
                 </div>
             </div>
-            <?php
+    <?php
         }
-        ?>
-    </div>
+    ?>
+</div>
